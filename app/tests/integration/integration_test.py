c16f9a259bfe57a6eb9b521580c30efba9041eaf
Sreenath Reddy <131184043+ZRD2KOR@users.noreply.github.com>
2023-11-24 16:00:55 +0530

    Updated integration test

--- app/tests/integration/integration_test.py
+++ app/tests/integration/integration_test.py
@@ -12,8 +12,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import json
-
 import pytest
 from velocitas_sdk.test.inttesthelper import IntTestHelper
 from velocitas_sdk.test.mqtt_util import MqttClient
@@ -37,118 +35,7 @@ async def test_set_position_not_allowed():
     assert len(response.errors) == 0
 
     response = mqtt_client.publish_and_wait_for_response(
-        request_topic=REQUEST_TOPIC, response_topic=RESPONSE_TOPIC, payload=payload
+        request_topic=REQUEST_TOPIC,
+        response_topic=RESPONSE_TOPIC,
+        payload=payload,
     )
-
-    assert response != ""
-
-    body = json.loads(response)
-    error_msg = f"""Not allowed to move seat because vehicle speed
-                is {float(speed_value)} and not 0"""
-    assert body["requestId"] == request_id
-    assert body["result"]["status"] == 1
-    assert body["result"]["message"] == error_msg
-
-
-# If you want to run the following integration tests,
-# make sure the feedercan is not running in your runtime.
-
-# @pytest.mark.asyncio
-# async def test_set_position_allowed():
-#     mqtt_client = MqttClient()
-#     inttesthelper = IntTestHelper()
-#     request_id = 123
-
-#     payload = {"position": 0, "requestId": request_id}
-
-#     response = await inttesthelper.set_float_datapoint(name="Vehicle.Speed", value=0)
-
-#     assert len(response.errors) == 0
-
-#     response = mqtt_client.publish_and_wait_for_response(
-#         request_topic=REQUEST_TOPIC,
-#         response_topic=RESPONSE_TOPIC,
-#         payload=payload,
-#     )
-
-#     body = json.loads(response)
-#     assert body["result"]["status"] == 0
-
-#     await sleep(1)
-
-#     position = 200
-#     payload = {"position": position, "requestId": request_id}
-
-#     response = mqtt_client.publish_and_wait_for_property(
-#         request_topic=REQUEST_TOPIC,
-#         response_topic="seatadjuster/currentPosition",
-#         payload=payload,
-#         path=["position"],
-#         value=position,
-#     )
-
-#     assert response != ""
-
-#     body = json.loads(response)
-#     assert body["position"] == position
-
-
-# @pytest.mark.asyncio
-# async def test_set_position_lt_0():
-#     mqtt_client = MqttClient()
-#     inttesthelper = IntTestHelper()
-#     request_id = 123
-#     seat_position = -1
-#     payload = {"position": seat_position, "requestId": request_id}
-
-#     response = await inttesthelper.set_float_datapoint(name="Vehicle.Speed", value=0)
-
-#     assert len(response.errors) == 0
-
-#     response = mqtt_client.publish_and_wait_for_response(
-#         request_topic=REQUEST_TOPIC,
-#         response_topic=RESPONSE_TOPIC,
-#         payload=payload,
-#     )
-
-#     assert response != ""
-
-#     body = json.loads(response)
-#     error_msg = (
-#         f"Failed to set the position {seat_position}, "
-#         f"error: Value out of range: {seat_position}"
-#     )
-#     assert body["requestId"] == request_id
-#     assert body["result"]["status"] == 1
-#     assert body["result"]["message"] == error_msg
-
-
-# @pytest.mark.asyncio
-# async def test_set_position__gt_1000():
-#     mqtt_client = MqttClient()
-#     inttesthelper = IntTestHelper()
-#     request_id = 123
-#     seat_position = 10000000001
-#     payload = {"position": seat_position, "requestId": request_id}
-
-#     response = await inttesthelper.set_float_datapoint(name="Vehicle.Speed", value=0)
-
-#     assert len(response.errors) == 0
-
-#     response = mqtt_client.publish_and_wait_for_response(
-#         request_topic=REQUEST_TOPIC,
-#         response_topic=RESPONSE_TOPIC,
-#         payload=payload,
-#     )
-
-#     assert response != ""
-
-#     body = json.loads(response)
-#     error_msg = (
-#         f"Failed to set the position {seat_position}, "
-#         f"error: Value out of range: {seat_position}"
-#     )
-
-#     assert body["requestId"] == request_id
-#     assert body["result"]["status"] == 1
-#     assert body["result"]["message"] == error_msg
